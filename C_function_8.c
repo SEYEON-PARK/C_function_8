f5f2175ccce2af85a81dbcf3cf7e5cfd55d2bd00
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-03-18 00:18:41 +0900

    C언어 function(함수) 연습!(8)

--- C_function_8.c
+++ C_function_8.c
@@ -7,7 +7,7 @@ int main(void)
 	char name[30]; // name 변수 생성
 
 	printf("당신의 이름을 입력하시오. : ");
-	scanf_s("%s", name, sizeof(name));
+	scanf_s("%s", name, sizeof(name)); // 사용자로부터 이름 입력받기
 
 	print(name);
 
