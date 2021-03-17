c0461140e2db04e954d0dab95d43b18b6b44e8e8
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-03-18 00:19:19 +0900

    C언어 function(함수) 연습!(8)

--- C_function_8.c
+++ C_function_8.c
@@ -9,12 +9,12 @@ int main(void)
 	printf("당신의 이름을 입력하시오. : ");
 	scanf_s("%s", name, sizeof(name)); // 사용자로부터 이름 입력받기
 
-	print(name);
+	print(name); // print 함수를 이용하여 이름 출력하기
 
 	return 0;
 }
 
 void print(char a[])
 {
-	printf("당신의 이름은 %s입니다.\n", a);
+	printf("당신의 이름은 %s입니다.\n", a); // 출력하기
 }
